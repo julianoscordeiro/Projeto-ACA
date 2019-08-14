012db2ff6c53535a6d2952b0fa8e46b6710acbf4
fernandohous <fernando.b10@aluno.ifsc.edu.br>
2019-08-14 17:24:22 -0300

    Adicionado arquivos de tema do bootstrap

--- login.php
+++ login.php
@@ -5,25 +5,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet">
+
+    <!-- Icons -->
+    <link href="/assets/vendor/nucleo/css/nucleo-icons.css" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link type="text/css" href="/assets/css/blk-design-system.min.css" rel="stylesheet">
+
+    <!-- Responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <style type="text/css">
+        #logincss{
+            margin: 10% 30% 30% 30%;
+        }
+    </style>
+        
+        
+    </head>
 
 <body>
 
 
 <!-- Formulario de login-->
-    <form>
-    <input placeholder="Email" type="text" name="email"><br>
-    <input placeholder="password" name="senha"><br>
-    <a href="cadastro.php" >Cadastrar-se </a>
-    <a href="resetpass.php" >Esqueci a senha</a><br>
+    
+    <form id="logincss">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email</label>
+            <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
+            
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Senha</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+        </div>
 
-    <input type="submit" value="Entrar">
+        <a href="cadastro.php" >Cadastrar-se </a>
+        <a href="resetpass.php" >Esqueci a senha</a><br>
+
+        <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
 
-    
+    <!-- Core -->
+    <script src="/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="/assets/vendor/popper/popper.min.js"></script>
+    <script src="/assets/vendor/bootstrap/bootstrap.min.js"></script>
+
+    <!-- Theme JS -->
+    <script src="/assets/js/blk-design-system.min.js"></script>
+
 </body>
 </html>
 
 <?php
+
+?>
 
