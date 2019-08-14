b261abb4cc7907a2b4bfa8e2d6ab830adb415bc6
fernandohous <fernando.b10@aluno.ifsc.edu.br>
2019-08-14 16:36:29 -0300

    Formularios login/Registro/Esqueci a senha

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+<!-- Formulario de login-->
+    <form>
+    <input placeholder="Email" type="text" name="email"><br>
+    <input placeholder="password" name="senha"><br>
+    <a href="cadastro.php" >Cadastrar-se </a>
+    <a href="resetpass.php" >Esqueci a senha</a><br>
+
+    <input type="submit" value="Entrar">
+    </form>
+
+    
+</body>
+</html>
+
+<?php
+
