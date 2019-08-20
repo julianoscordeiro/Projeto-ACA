1e405c3965067ee8805d364f32136e21226c8d5c
Aluno <aluno@tub-depe-a0725.tubarao.ifsc.edu.br>
2019-08-20 19:36:47 -0300

    Criação da pasta de arquivos antigos

---
+++ arquivosAntigos/template.php
@@ -0,0 +1,83 @@
+<?php
+
+function printHeader($titulo, $css = null){
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet">
+
+    <!-- Icons -->
+    <link href="/assets/vendor/nucleo/css/nucleo-icons.css" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    
+
+    <!-- Theme CSS -->
+    <link type="text/css" href="/assets/css/blk-design-system.min.css" rel="stylesheet">
+
+    <!-- Responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" type="text/css" href="..\style.css" />
+
+    <?php
+    if($css){
+
+    
+        foreach ($css as $one) {
+            echo " <link rel='stylesheet' type='text/css''href='$one' /> ";
+        }
+
+    }
+    
+    
+    
+    ?>
+
+    <title><?php echo "$titulo"; ?> | ACA - Aplicativo de correção automatizada</title>
+    
+</head>
+<body>
+
+<nav id="menunav" class="navbar fixed-top navbar-light ">
+  <div class="container">
+  <div class="card card-nav-tabs card-plain">
+    <div class="card-header card-header-danger">
+        <!-- colors: "header-primary", "header-info", "header-success", "header-warning", "header-danger" -->
+        <div class="nav-tabs-navigation">
+            <div class="nav-tabs-wrapper">
+                <ul class="nav nav-tabs" data-tabs="tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="..\index.php" data-toggle="tab">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login/login.php" data-toggle="tab">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login/cadastro.php" data-toggle="tab">Cadastrar</a>
+                    </li>
+                    <li>
+                        
+                    </li>
+                </ul>
+            </div>
+        </div>
+  </div>
+  </div>
+</nav>
+
+
+
+
+
+
+    
+</body>
+</html> 
+
+
+<?php
+
+}
