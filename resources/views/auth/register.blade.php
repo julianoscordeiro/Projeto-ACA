20913543d53c37255dabf3c00b4ed402bb48ad59
fernandohous <fernando.b10@aluno.ifsc.edu.br>
2019-08-21 20:05:44 -0300

    Atualização de usuario

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div >
+            <div>
                 
 
                 <div class="card-body" id="formlogincss">
