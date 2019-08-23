14313d5fa929573b805d15cfa628ee2218f00eb1
fernandohous <fernando.b10@aluno.ifsc.edu.br>
2019-08-23 19:47:40 -0300

    Atualização design painel

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center ">
         <div class="col-md-12">
             <div class="card">
-                <div class="card "></div>
+                <div class=" "></div>
 
                 <div class="card-body fundoContent ">
                     @if (session('status'))
@@ -14,7 +14,8 @@
                         </div>
                     @endif
 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas imperdiet non tellus et lobortis. Duis neque arcu, tempor sed ornare non, aliquam vel tellus. Aliquam placerat lobortis elit eget aliquam. Praesent lacinia sagittis leo, eu porta ex blandit eget. Duis semper sit amet nulla eget luctus. Ut dignissim dolor accumsan massa sodales maximus. Donec pretium urna at dapibus faucibus. Mauris molestie fermentum leo, vel facilisis neque iaculis in. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam lorem justo, varius non fringilla at, eleifend et metus. In felis elit, dictum mattis nisi eget, accumsan efficitur dui. Etiam diam nulla, faucibus at iaculis sit amet, luctus et turpis.
+                    Bem vindo ao ACA!
+
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -34,16 +34,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}" />
+    
 </head>
 <body class="fundoDashboard">
 
 
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light fundoNavBarDashboard shadow-sm">
+        
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
                     ACA
                 </a>
+
+            
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -51,8 +55,39 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                                <!-- Menus -->
 
+
+                        <div class="card card-nav-tabs card-plain">
+                            <div class="card-header card-header-danger">
+                                <!-- colors: "header-primary", "header-info", "header-success", "header-warning", "header-danger" -->
+                                <div class="nav-tabs-navigation">
+                                    <div class="nav-tabs-wrapper">
+                                        <ul class="nav nav-tabs" data-tabs="tabs">
+                                            <li class="nav-item">
+                                                <a class="nav-link active" href="#home" data-toggle="tab">Inicio</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#updates" data-toggle="tab">Questões</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#history" data-toggle="tab">Provas</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#history" data-toggle="tab">Turmas</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#history" data-toggle="tab">Correção</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#history" data-toggle="tab">Relatórios</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                        </div>
                     </ul>
+                    
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
@@ -68,7 +103,7 @@
                             @endif
                         @else
                             <div class="nav-item  dropdown">
-                                <a id="navbarDropdown" class=" dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class=" dropdown-toggle btn btn-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
