22cadb8039cd4356fb0828c88c809049d5302b28
julianoscordeiro <julianoscordeiro@gmail.com>
2019-08-27 16:43:37 -0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,13 +9,13 @@
                 
             
                 <div class="card-body" id="formlogincss">
-                <h1 class="display-1 ">Entrar:</h1>
+                <h1 class="display-1 ">Acesse sua conta:</h1>
                     
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-mail') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-mail:') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -29,7 +29,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha:') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -40,35 +40,25 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Lembrar-me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
+                        </div>                  
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
+                             
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Entrar') }}
                                 </button>
-                                
-                                <a href="/register"  class="btn btn-primary">
-                                    {{ __('Registrar-se') }}
-                                </a>
-
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Esqueci da senha') }}
+                                        {{ __('Recuperar senha') }}
                                     </a>
-                                @endif
+                                @endif  
+                                <br> 
+                                <a>Não possui uma conta?</a>                               
+                                <a href="/register">
+                                    {{ __('Cadastre-se') }}
+                                </a>
+                              
+                               
                             </div>
                         </div>
                     </form>
