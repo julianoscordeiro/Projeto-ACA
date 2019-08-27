489d4d2abec15bc0069735a4ece70deb5620faa8
julianoscordeiro <julianoscordeiro@gmail.com>
2019-08-27 16:43:41 -0300

    Update email.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -8,7 +8,7 @@
                 
                 
                 <div class="card-body" id="formlogincss">
-                <h1 class="display-1 ">Recuperar senha:</h1>
+                <h1 class="display-1 ">Recupere sua senha:</h1>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -19,7 +19,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-mail:') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -35,7 +35,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Enviar email') }}
+                                    {{ __('Enviar Email') }}
                                 </button>
                             </div>
                         </div>
