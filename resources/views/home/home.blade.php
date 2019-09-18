07c876fb26405a92b1ff8c7d438fd731c943c98d
julianoscordeiro <julianoscordeiro@gmail.com>
2019-09-18 17:05:31 -0300

    Update home.blade.php

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -4,7 +4,7 @@
                 
                 <!-- Texto de boas vindas -->
                     <div class="textocenter">
-                        <p class="display-1">Olá Professor,</p>
+                        <p class="display-1">Olá Professor</p>
                         <p class="display-3">Seja bem-vindo!</p>
                         <h2>
                             <small class="text-muted">                     
@@ -44,35 +44,18 @@
                         <div class="carousel-inner">                        
                             <div class="carousel-item active">
                                 <img class="w3-card-4" src="https://user-images.githubusercontent.com/53874458/65147675-02480500-d9f5-11e9-874b-e783a97181c0.png" alt="First slide" id="centro">
-                                    <div class="carousel-caption">
-                                        <h5>Crie seu Banco de Questões</h5>
-                                        <p>Na aba questões crie, armazene e gerencie facilmente quantas exercícios você desejar.</p>    
-                                </div>  
                             </div> 
                             
                             <div class="carousel-item">
-                            <img class="w3-card-4" src="https://user-images.githubusercontent.com/53874458/65147584-d75db100-d9f4-11e9-885c-611b61904844.png" alt="Second slide" id="centro">
-                                <div class="carousel-caption">
-                                    <h5>Crie Provas Únicas</h5>
-                                    <p>Na aba provas crie avaliações personalizadas com questões diferentes para suas turmas. Suas provas criadas podem ser encontradas com rapidez e organização.</p>    
-                                </div>    
+                            <img class="w3-card-4" src="https://user-images.githubusercontent.com/53874458/65147584-d75db100-d9f4-11e9-885c-611b61904844.png" alt="Second slide" id="centro">    
                             </div>
     
                             <div class="carousel-item">
                             <img class="w3-card-4" src="https://user-images.githubusercontent.com/53874458/65147750-286da500-d9f5-11e9-84e1-5f1a0b3af299.png" alt="Third slide" id="centro">
-                                <div class="carousel-caption">
-                                    <h5>Gerencie suas Turmas</h5>
-                                    <p>Na aba Turmas adicione seus respectivos alunos ou importe sua lista de nomes em formato .CSV, .XLSX. Vincule suas provas as classes criadas para impressão e aplicação.</p>    
-                                </div>    
                             </div>
                             
                             <div class="carousel-item">
-                            <img class="w3-card-4" src="https://user-images.githubusercontent.com/53874458/65147635-f1978f00-d9f4-11e9-809c-ac1c657b890d.png" alt="Fourty slide" id="centro">
-                                <div class="carousel-caption">
-                                    <h5>Corrija suas provas</h5>
-                                    <p>Na aba Correções cadastre os gabaritos das provas aplicadas de seus alunos e efetue a correção automatizada e receba o relatório de notas da turma. Diminua substancialmente as horas de correção de exames.</p>    
-                                </div>
-                            </div>                 
+                            <img class="w3-card-4" src="https://user-images.githubusercontent.com/53874458/65147635-f1978f00-d9f4-11e9-809c-ac1c657b890d.png" alt="Fourth slide" id="centro">               
                         </div>
                         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
