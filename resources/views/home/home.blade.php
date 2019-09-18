93872a7b812d43aba30ae00e41a2ed38af1c3cea
julianoscordeiro <julianoscordeiro@gmail.com>
2019-09-18 16:11:50 -0300

    Update home.blade.php

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -48,8 +48,8 @@
                                         <h5>Crie seu Banco de Questões</h5>
                                         <p>Na aba questões crie, armazene e gerencie facilmente quantas exercícios você desejar.</p>    
                                 </div>  
-                            </div>  
- 
+                            </div> 
+                            
                             <div class="carousel-item">
                             <img class="w3-card-4" src="https://user-images.githubusercontent.com/53874458/65147584-d75db100-d9f4-11e9-885c-611b61904844.png" alt="Second slide" id="centro">
                                 <div class="carousel-caption">
@@ -57,7 +57,7 @@
                                     <p>Na aba provas crie avaliações personalizadas com questões diferentes para suas turmas. Suas provas criadas podem ser encontradas com rapidez e organização.</p>    
                                 </div>    
                             </div>
-                          
+    
                             <div class="carousel-item">
                             <img class="w3-card-4" src="https://user-images.githubusercontent.com/53874458/65147750-286da500-d9f5-11e9-84e1-5f1a0b3af299.png" alt="Third slide" id="centro">
                                 <div class="carousel-caption">
