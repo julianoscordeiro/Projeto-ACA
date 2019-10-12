59661e818983cf09db26bdbda768beffd9182c76
julianoscordeiro <julianoscordeiro@gmail.com>
2019-10-12 15:27:32 -0300

    Criando Controller para importar e exportar listagem

---
+++ app/Http/Controllers/ExcelController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Excel;
+use DB;
+
+class ExcelController extends Controller
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function importExport()
+    {
+       return view('import');
+    }
+    
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function export() 
+    {
+        return Excel::download(new ExportUsers, 'users.xlsx');
+    }
+    
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function import() 
+    {
+        Excel::import(new ImportUsers, request()->file('file'));
+            
+        return back();
+    }
+}
